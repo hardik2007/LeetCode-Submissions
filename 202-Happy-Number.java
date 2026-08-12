516d3fed9af19f6102317884c5f508ed2e176e91
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-08-12 18:31:30 +0530

    [202] [Time Beats: 77.46%] [Memory Beats: 51.44%] - LeetPush

---
+++ 202-Happy-Number.java
@@ -0,0 +1,22 @@
+class Solution {
+    private int findSq(int n){
+        int ans = 0;
+        while(n>0){
+            int rem = n%10;
+            ans+=rem*rem;
+            n/=10;
+        }
+        return ans;
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = findSq(slow);
+            fast = findSq(findSq(fast));
+        }while(slow!=fast);
+
+        return slow == 1;
+}
+}
