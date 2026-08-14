11cc0b29c392b345bf3f4919aeed118eb8a542ee
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-08-14 14:45:03 +0530

    [61] [Time Beats: 100.00%] [Memory Beats: 63.42%] - LeetPush

---
+++ 61-Rotate-List.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null){
+            return head;
+        }
+        int c = 1;
+        ListNode temp = head;
+        while(temp.next!=null){
+            c++;
+            temp = temp.next;
+        }
+        temp.next = head;
+        temp = head;
+        k = k%c;
+        for(int i=0;i<c-k-1;i++){
+            temp = temp.next;
+        }
+        head = temp.next;
+        temp.next = null;
+
+        return head;
+    }
+}
