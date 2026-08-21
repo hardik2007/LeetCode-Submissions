7fdf61ea4bc8cd444936d8d810bc34546cfca66c
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-08-22 00:44:41 +0530

    [921] [Time Beats: 41.97%] [Memory Beats: 77.72%] - LeetPush

---
+++ 921-Minimum-Add-to-Make-Parentheses-Valid.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                stack.push(ch);
+            }else if(ch == ')'){
+                if(!stack.isEmpty()&&stack.peek() == '('){
+                stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+            }
+        }
+        return stack.size();
+    }
+}
