12ce64aa61da25ca06356ea66a057ecf67864d20
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-08-22 17:14:26 +0530

    [852] [Time Beats: 100.00%] [Memory Beats: 40.32%] - LeetPush

---
+++ 852-Peak-Index-in-a-Mountain-Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s = 1;
+        int e = arr.length -2;
+
+        while(s<=e){
+            int m = s+(e-s)/2;
+
+            if(arr[m] > arr[m-1] && arr[m] > arr[m+1]){
+                return m;
+            }else{
+                if(arr[m] < arr[m+1]){
+                    s = m+1;
+                }else{
+                    e = m-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
