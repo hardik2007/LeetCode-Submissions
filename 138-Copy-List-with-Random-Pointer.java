57c9328ee3b2da5319e2e3ac0487a555ab40f7ba
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-08-23 22:11:26 +0530

    [138] [Time Beats: 100.00%] [Memory Beats: 88.24%] - LeetPush

---
+++ 138-Copy-List-with-Random-Pointer.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Node curr = head;
+
+        while(curr!=null){
+            Node clone = new Node(curr.val);
+            clone.next = curr.next;
+            curr.next = clone;
+            curr = clone.next;
+        }
+
+        curr = head;
+        while(curr!=null){
+            if(curr.random!=null){
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+        curr = head;
+        Node cloneHead = curr.next;
+        Node cloneCur = cloneHead;
+        while(curr!=null){
+            curr.next = curr.next.next;
+            if(cloneCur.next!=null){
+                cloneCur.next = cloneCur.next.next;
+            }
+            curr = curr.next;
+            cloneCur = cloneCur.next;
+        }
+        return cloneHead;
+    }
+}
