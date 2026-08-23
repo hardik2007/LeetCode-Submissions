5399a0b60e37bb11eadf8514cd1b4e6cdd6d0e77
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-08-23 17:22:32 +0530

    [206] [Time Beats: 100.00%] [Memory Beats: 68.44%] - LeetPush

--- 206-Reverse-Linked-List.java
+++ 206-Reverse-Linked-List.java
@@ -10,22 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
-        }
         ListNode prev = null;
         ListNode curr = head;
-        ListNode next = head.next;
 
-        while(curr != null){
+        while(curr!=null){
+            ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = next;
-            if(next!= null){
-            next = next.next;
-            }
+            curr=next;
         }
-        head = prev;
-        return head;
+        return prev;
     }
 }
