dde04e85fcbf60b86fc040360574478cf7213d5c
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-09-03 12:56:16 +0530

    [189] [Time Beats: 22.45%] [Memory Beats: 12.35%] - LeetPush

---
+++ 189-Rotate-Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void reverse(vector<int>& arr, int s, int e){
+    while(s<e){
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+        s++;
+        e--;
+    }
+}
+    void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    if(n==0 || k==0){
+        return;
+    }
+    k = k%n;
+    reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,n-1);
+    }
+};
