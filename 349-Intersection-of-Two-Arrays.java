31759d2bdccd84f172d98c5c8096150829c39648
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-09-08 19:16:23 +0530

    [349] [Time Beats: 97.44%] [Memory Beats: 25.29%] - LeetPush

--- 349-Intersection-of-Two-Arrays.java
+++ 349-Intersection-of-Two-Arrays.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        int[] output = new int[(nums1.length + nums2.length)];
-        int n = nums1.length;
-        for(int i=0;i<n;i++){
-            if(!map1.containsKey(nums1[i])){
-                map1.put(nums1[i], 1);
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> intersection = new HashSet<>();
+
+        for(int val : nums1){
+            set.add(val);
+        }
+
+        for(int val : nums2){
+            if(set.contains(val)){
+                intersection.add(val);
             }
         }
-        n = nums2.length;
+        int[] output = new int[intersection.size()];
         int k = 0;
-        for(int i=0;i<n;i++){
-            if(map1.containsKey(nums2[i])){
-                output[k++] = nums2[i];
-                map1.remove(nums2[i]);
-            }
+        for(int num : intersection){
+            output[k++] = num;
         }
-        int[] res = new int[k];
-        for(int i=0;i<k;i++){
-            res[i] = output[i];
-        }
-        return res;
+        return output;
     }
 }
