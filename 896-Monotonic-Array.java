5794b6799d7a8094658c0557e13a439775a820fc
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-09-13 21:43:00 +0530

    [896] [Time Beats: 95.52%] [Memory Beats: 39.22%] - LeetPush

---
+++ 896-Monotonic-Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+            int n = nums.length;
+            boolean inc = true;
+            boolean dec = true;
+            for(int i=0;i<n-1;i++){
+                if (!inc && !dec) {
+                return false;
+                }
+                if(nums[i] < nums[i+1]){
+                    dec= false;
+                }
+                if(nums[i] > nums[i+1]){
+                    inc = false;
+                }
+            }
+            return inc || dec;
+        }
+    }
