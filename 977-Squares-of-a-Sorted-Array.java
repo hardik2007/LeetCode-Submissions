b0f55ae24614fa202763453834686b4f6f861006
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-09-13 21:33:10 +0530

    [977] [Time Beats: 100.00%] [Memory Beats: 52.04%] - LeetPush

--- 977-Squares-of-a-Sorted-Array.java
+++ 977-Squares-of-a-Sorted-Array.java
@@ -1,10 +1,25 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
-        for(int i=0;i<n;i++){
-            nums[i] = nums[i]*nums[i];
+        int[] res = new int[n];
+
+        int s = 0;
+        int e = n-1;
+        int idx = n-1;
+
+        while(s<=e){
+            int ls = nums[s]*nums[s];
+            int rs = nums[e]*nums[e];
+
+            if(ls>rs){
+                res[idx] = ls;
+                s++;
+            }else{
+                res[idx] = rs;
+                e--;
+            }
+            idx--;
         }
-        Arrays.sort(nums);
-        return nums;
+        return res;
     }
 }
