b0c053b3e38414395dfa4f4428ea5b739af06ecc
Hardik Bhagtani <70148436+hardik2007@users.noreply.github.com>
2026-09-19 00:06:00 +0530

    [617] [Time Beats: 100.00%] [Memory Beats: 34.91%] - LeetPush

---
+++ 617-Merge-Two-Binary-Trees.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1==null && root2 == null){
+            return null;
+        }
+        int v1 = 0;
+        if(root1!=null){
+            v1 = root1.val;
+        }
+        int v2 = 0;
+        if(root2!=null){
+            v2 = root2.val;
+        }
+
+        TreeNode root = new TreeNode(v1+v2);
+        if(root1==null) return root2;
+        if(root2==null) return root1;
+        
+        root.left = mergeTrees(root1.left,root2.left);
+        root.right = mergeTrees(root1.right,root2.right);
+
+        return root;
+    }
+}
